ba65d31641f7a1d38b38c5dae9dfce68ea0194ab
Fernanda Pinho <fbp@ecomp.poli.br>
2023-02-16 14:23:59 -0300

    .gitignore is now working

--- src/consts/consts.ts
+++
@@ -1,3 +0,0 @@
-export const REACT_APP_API_KEY="69a37bff5bmshf2b6946a838cdbfp1636f4jsn83d15f7bc166"
-export const REACT_APP_API_HOST="moviesdatabase.p.rapidapi.com"
-export const REACT_APP_API_URL="https://moviesdatabase.p.rapidapi.com/titles"
